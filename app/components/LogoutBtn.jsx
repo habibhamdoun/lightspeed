e74587a7d22eb3a866f7a3a29635bef9f2375318
habibhamdoun <habibhamdoun@gmail.com>
2024-04-02 22:48:37 +0300

    implmeneting firebase

--- app/components/LogoutBtn.jsx
+++ app/components/LogoutBtn.jsx
@@ -1,8 +1,14 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
+import { auth } from '../config/firebase';
 
 const LogoutBtn = () => {
   const logout = async (e) => {
-    // TODO: Masrshoud put ur function here
+    try {
+      await signOut(auth);
+    } catch (err) {
+      console.log(err);
+    }
   };
   return (
     <button onClick={logout} className='mt-5 text-red-500 underline'>
